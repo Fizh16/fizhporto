3f592b39c6ecaa32f45de42678b1f36ed18330bf
Fizh16 <76116714+Fizh16@users.noreply.github.com>
2021-11-21 23:00:02 +0700

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,104 +1,111 @@
-
-let slideRight = {
-    distance: '100%',
-    origin: 'left',
-    easing: 'ease-in',
-    opacity: 0,
-    duration: 600,
-};
-
-let popUp = {
-    easing: 'ease-in',
-    duration: 600,
-};
-
-let slideDown = {
-    distance: '100%',
-    origin: 'top',
-    easing: 'ease-in',
-    opacity: 0,
-    duration: 500,
-    mobile: false
-};
-
-const delay1 = [300, 500];
-const class1 = ['.title', '.sub-title']
-
-const delaySkills = [600, 700, 800, 900, 1000];
-const classSkills = ['.php', '.python', '.html', '.js', '.css'];
-
-function responsive(){
-    if ($(window).width() <= 991){
-        $('#navbarNav').addClass('collapse navbar-collapse')
-        $('#navItem').removeClass('justify-content-end')
-        $('.nav-link').removeClass('fs-4')
-        $('.navbar-brand').removeClass('fs-1')
-        $('.skill-box').removeClass('container')
-        $('.skill-box').attr('style', 'width: $(window).width();')
-    } else {
-        $('#navbarNav').removeClass('collapse navbar-collapse')
-        $('#navItem').addClass('justify-content-end')
-        $('.nav-link').addClass('fs-4')
-        $('.navbar-brand').addClass('fs-1')
-        $('.skill-box').addClass('container')
-    }
-
-    if ($(window).width() <= 350){
-        $('.group-skill').removeClass('justify-content-end')
-        $('#col-skill').removeClass('col')
-    } else {
-        $('#col-skill').addClass('col')
-    }
-}
-
-function animation(){
-    ScrollReveal().reveal('.navbar-brand', slideRight);
-    ScrollReveal().reveal('.nav-link1', slideDown)
-    setTimeout(function(){
-        ScrollReveal().reveal('.nav-link2', slideDown);
-    }, 200)
-    setTimeout(function(){
-        ScrollReveal().reveal('.nav-link3', slideDown);
-    }, 300)
-    setTimeout(function(){
-        ScrollReveal().reveal('.nav-link4', slideDown);
-    }, 400)
-    ScrollReveal().reveal('.introTxt', slideRight);
-    ScrollReveal().reveal('.btn', slideRight);
-    ScrollReveal().reveal('.avatar', popUp);
-
-    ScrollReveal().reveal('.skill-box', {
-        distance: '100%',
-        origin: 'top',
-        easing: 'ease-in',
-        opacity: 0,
-        duration: 500,
-    });
-    
-    for (i = 0; i < class1.length; i++){
-        ScrollReveal().reveal(class1[i], {
-            distance: '100%',
-            origin: 'top',
-            easing: 'ease-in',
-            duration: 500,
-            delay: 300*i
-        });
-    }
-
-    for (i = 0; i < classSkills.length; i++){
-        ScrollReveal().reveal(classSkills[i], {
-            easing: 'ease-in',
-            duration: 600,
-            delay: 500 * i
-        });
-    }
-}
-
-animation();
-responsive();
-
-$( window ).resize(function() {
-    responsive();
-    $('.nav-link').attr('style', 'visibility: visible;')
-    $('.skill-box').attr('style', 'visibility: visible;')
+// Landing Page Animation
+ScrollReveal().reveal('#title-name', { duration : 1500 });
+ScrollReveal().reveal('#sub-title', {
+    duration : 1500,
+    delay : 300
 });
+
+// Projects Page Animation
+ScrollReveal().reveal('#menu1', { duration : 1500 });
+ScrollReveal().reveal('#title-project1', { 
+    duration : 1500,
+    delay : 200
+});
+ScrollReveal().reveal('#img-project-id', { 
+    duration : 1500,
+    delay : 200
+});
+ScrollReveal().reveal('#sub-title-project1', { 
+    duration : 1500,
+    delay : 400
+});
+ScrollReveal().reveal('#btn-project', { 
+    duration : 1500,
+    delay : 600
+});
+
+ScrollReveal().reveal('#menu2', { duration : 1500 });
+ScrollReveal().reveal('#about', { 
+    duration : 1500,
+    delay : 200
+});
+ScrollReveal().reveal('#languages', { 
+    duration : 1500,
+    delay : 400
+});
+ScrollReveal().reveal('#contact', { 
+    duration : 1500,
+    delay : 600
+});
+
+(function ( $ ) {
+    $.fn.replaceClass = function(c1, c2) {
+        return this.removeClass(c1).addClass(c2);
+    };
+}( jQuery ));
+
+//parallax
+$(window).scroll(function () {
+    let scrollY = $(this).scrollTop();
+
+    if (scrollY > 200 && scrollY < 1224){
+        if($('#bg-project').css( 'opacity' ) == 0){
+            $('#bg-project').stop().fadeTo( 500, 1 )
+        }
+    } else {
+        if($('#bg-project').css( 'opacity' ) == 1){
+            $('#bg-project').stop().fadeTo( 500, 0 )
+        }
+    }
+
+    if (scrollY > 100) {
+        $('.navbar-brand').css('visibility', 'hidden');
+        $('.navbar').removeClass('background-light');
+    } else {
+        $('.navbar-brand').css('visibility', '');
+        $('.navbar').addClass('background-light');
+    }
+
+    if (scrollY > 720 && scrollY < 1401) {
+        $('.navbar').removeClass('navbar-light');
+        $('.navbar').addClass('navbar-dark');
+    } else {
+        $('.navbar').removeClass('navbar-dark');
+        $('.navbar').addClass('navbar-light');
+    }
+    
+    $('#title').css({
+        'transform' : 'translate(0px, '+ scrollY/1.2 +'%)'
+    });
+
+    $('.img-project').css('top', scrollY * 0.2 + 'px');
+    $('.text-project').css('top', scrollY * 0.3 + 'px');
+})
+
+// Change Background
+$('.carousel-button').click(function(){
+    if($('#carousel1').hasClass('active')){
+        $('#bg-project').css('background-image', 'url(assets/img/palang_web_design.jpg)');
+    } else {
+        $('#bg-project').css('background-image', 'url(assets/img/anime_web_design.jpg)');
+    }
+});
+
+//responsive
+if ($(window).width() <= 750){
+    $('.col-image').replaceClass('text-start', 'text-center')
+    $('.nav').removeClass('navbar-nav');
+    $('.nav-link').css('color', '#3C3C3C')
+    $('#about').replaceClass('fs-4', 'fs-5')
+    $('.col-about').replaceClass('col', 'text-center')
+    $('hr:first').width('200px')
+    $('hr').width('100px')
+    $('.navbar').removeClass('sticky-top')
+    $('.col-project').removeClass('col')
+    $('.nav-link').replaceClass('font-20', 'fs-6')
+} else {
+    $('.col-image').replaceClass('text-center', 'text-start')
+    $('.nav').addClass('navbar-nav');
+    $('#about').replaceClass('fs-5', 'fs-4')
+    $('.col-about').replaceClass('text-center', 'col')
+}
